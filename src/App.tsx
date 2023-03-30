eb38435a2ebe57e035e17b812ec8fcc8e506f7e8
Marcelo Victor <marcelovictorsm077@gmail.com>
2023-03-30 14:33:42 -0300

    Generics

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
-import { Post } from "./components/Post"
+import { Post, PostType } from "./components/Post"
 import { Header } from "./components/Header"
 import { Sidebar } from "./components/Sidebar"
-import { Comment } from "./components/Comment"
 
 import styles from "./App.module.css"
 
 import "./global.css"
 
 
-const posts =[
+const posts: PostType[]=[
   {
     id: 1,
     author: {
@@ -53,9 +52,7 @@ export function App() {
             return (
               <Post 
                 key={post.id}
-                author={post.author}
-                content={post.content}
-                publishedAt={post.publishedAt}
+                post={post}
               />
             )
           })}
