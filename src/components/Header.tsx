a89825667fed41cdcaa91c12b43af5a3641f8bf5
Marcelo Victor <95694179+marcelovdsm@users.noreply.github.com>
2023-07-27 23:17:00 +0000

    adjust

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import styles from './Header.module.css'
 
 import igniteLogo from '../assets/ignite-logo.svg'
 
-export const Header = (props) => {
+export const Header = () => {
   return(
     <header className={styles.header}>
       <img src={igniteLogo} alt="Logotipo do ignite" />
